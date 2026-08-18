e913984904aa2effc8d94c2ca32ee3f61dcc37d4
aminebenichou <65418932+amine-ds@users.noreply.github.com>
2026-08-18 12:25:06 +0200

    Convert Snowflake DataFrame to Pandas and display

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,8 @@ conn = st.connection("snowflake")
 session=conn.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('search_on'))
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 #st.dataframe(data=my_dataframe, use_container_width=True)
