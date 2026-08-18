befe9ba7e752554a6f7a72f00473c0b1cb0ce30c
aminebenichou <65418932+amine-ds@users.noreply.github.com>
2026-08-18 12:36:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ session=conn.session()
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your Smoothie will be :", name_on_order)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options")
 pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
