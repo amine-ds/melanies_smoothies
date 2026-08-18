ea492a98152d62a4229a6396477677e8cd47fde6
aminebenichou <65418932+amine-ds@users.noreply.github.com>
2026-08-18 12:08:09 +0200

    Integrate API call for smoothie ingredient data
    Added requests import and API call to fetch watermelon data. Updated smoothie order insertion logic to include selected ingredients.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+import requests  
 
 
 # Write directly to the app
@@ -27,13 +27,12 @@ ingredients_list = st.multiselect('Choose up to 5 ingredients', my_dataframe,max
 
 
 if ingredients_list:
-#    st.write(ingredients_list)
-#    st.text(ingredients_list)
-
-
     ingredients_string = ''
     for fruit_choose in ingredients_list:
         ingredients_string+=fruit_choose+ ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,NAME_ON_ORDER)
                     values ('""" + ingredients_string + """','"""+name_on_order+  """')"""
@@ -44,8 +43,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-#New section to display smoothiefroot nutrition information
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
