0a46d6206f0a392ca73f4083acd2d314ac45438c
aminebenichou <65418932+amine-ds@users.noreply.github.com>
2026-08-18 12:45:35 +0200

    Update dataframe display to filter by chosen fruit
    Filter displayed dataframe to show only the chosen fruit's search term.

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ st.write("The name on your Smoothie will be :", name_on_order)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
+st.dataframe(pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'])
 st.stop()
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients', my_dataframe,max_selections=5)
