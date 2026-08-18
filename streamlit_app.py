48ea2049ffcef4d7f02a78b58fe279509fa2a02d
aminebenichou <65418932+amine-ds@users.noreply.github.com>
2026-08-18 11:36:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 conn = st.connection("snowflake")
+session=conn.session()
 
 # Write directly to the app
 st.title(f"My Parrents New Healthy Dinner")
@@ -12,7 +13,7 @@ Kale, Spinach & Rocket Smoothie \n
 Hard-Boiled Free-Range Egg""")
 #option = st.selectbox('What is your favorites fruits?',('Banana','Strawberries','Peaches'))
 #st.write ('Your favorite fruit is :', option)
-my_dataframe = conn.query("SELECT * FROM smoothies.public.fruit_options;", ttl="10m")
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
