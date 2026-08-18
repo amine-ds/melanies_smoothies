21651219eb2db5f993807f947b2fca96186ad178
aminebenichou <65418932+amine-ds@users.noreply.github.com>
2026-08-18 12:47:53 +0200

    Implement ingredient selection with multiselect
    Add functionality to select ingredients and stop execution.

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,8 @@ pd_df=my_dataframe.to_pandas()
 #st.stop()
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients', my_dataframe,max_selections=5)
-
+ingredients_list
+st.stop()
 
 if ingredients_list:
     for fruit_chosen in ingredients_list:
